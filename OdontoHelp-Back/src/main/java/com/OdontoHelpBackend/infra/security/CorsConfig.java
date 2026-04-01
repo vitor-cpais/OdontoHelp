46b56fb974e3090580cb1d711b45554ca3e4c65d
Vitor C. Pais <60139322+vitor-cpais@users.noreply.github.com>
2026-03-31 22:31:53 -0300

    Update CorsConfig.java

--- OdontoHelp-Back/src/main/java/com/OdontoHelpBackend/infra/security/CorsConfig.java
+++ OdontoHelp-Back/src/main/java/com/OdontoHelpBackend/infra/security/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:4200", "http://localhost:5173") // Portas do angular e do react
+                .allowedOrigins("http://localhost:3000", "http://localhost:4200", "http://localhost:5173", "https://odonto-help.vercel.app") // Portas do angular e do react
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH");
     }
 }
